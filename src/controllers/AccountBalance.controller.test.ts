3d183d1dbb3a8580f80b874cffd5dfd75e4c4dc7
linh.nguyen <linh.nguyen@rolleaseacmeda.com>
2025-06-07 10:40:29 +1000

    Setup Testing for Controllers, Services and Repository

---
+++ src/controllers/AccountBalance.controller.test.ts
@@ -0,0 +1,9 @@
+import { AccountBalance } from "src/types/Balances.type"
+
+describe("AccountBalanceController", () => {
+    //Arrange
+    const controller = new AccountBalanceController
+    //Act
+
+    //Assert
+})
---
+++ src/repositories/AccountBalance.respository.test.ts
@@ -0,0 +1,5 @@
+describe('Account Balance Repository', () => {
+    it('', () => {
+
+    })
+})
